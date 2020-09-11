e103bce346304d9b915367c3dbd540310a2901de
rob <robertazzopardi@icloud.com>
2020-09-11 23:54:22 +0100

    removed testing files

---
+++ tests/assembly.asm
@@ -0,0 +1,16 @@
+%include 'functions.asm'
+
+global main
+extern printf
+
+section .data
+	sum DQ 0
+	integerfmt db '%d', 10, 0
+
+section .text
+
+main:
+	_add 1, 10, qword [sum]
+	print_sum qword [sum], integerfmt
+
+	ret
