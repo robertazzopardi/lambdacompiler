6263912819b5fa0e842a50855f9204c52ea692f3
rob <robertazzopardi@icloud.com>
2020-09-15 23:39:17 +0100

    configured launch json for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 
 // nasm -felf64 simple.asm && ld simple.o && ./a.out
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+    // std::cout << "test" << std::endl;
+
     fhandler::FileHandler::parseArgs(argc, argv);
 
     parser::Parser parser;
